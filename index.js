7286c7489afdaefd33441ee805dbc4cbd2043f71
key2-hue <keymaeda3782@gmail.com>
2020-02-19 20:06:26 +0900

    first commit

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
 
-  document.getElementById('num').textContent = "Hi";
+
+  var questions = ['日本の首都は？','中国の首都は？','韓国の首都は？','アメリカの首都は？'];
+  var answers = [
+    ['東京','大阪','福岡','愛知'],
+    ['北京','上海','広東','マカオ'],
+    ['ソウル','プサン','テグ','インチョン'],
+    ['ワシントンD.C.','ニューヨーク','ハワイ','ラスベガス'],
+  ]
+  const num = Math.floor(Math.random() * questions.length);
+  var question = document.createElement('div');
+  document.body.appendChild(question);
+  question.id = "question";
+  question.textContent = questions[num];
+
+  for (let i = 0; i < 4;i++) {
+    var answer = document.createElement('div');
+    document.body.appendChild(answer);
+    answer.id = 'answer answer' + i;
+    answer.className = i;
+    answer.textContent = answers[num][i];
+  }
+
+  
+
+  
+  
