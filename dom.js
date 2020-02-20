222f8b915d0b29b8499b109d321498df0d7624d8
key2-hue <keymaeda3782@gmail.com>
2020-02-20 20:30:27 +0900

    02/20 20:30

--- dom.js
+++ dom.js
@@ -1,5 +1,17 @@
 'use strict';
 
 {
-  
+  function good() {
+    document.querySelector('p').textContent =
+    '練習中';
+  }
+  setTimeout(good,2000);
+
+  function num() {
+    document.querySelectorAll('ul > li').forEach((li,index) => {
+      li.textContent = `練習${index}`;
+    });
+  }
+  setTimeout(num,1000);
+
 }
